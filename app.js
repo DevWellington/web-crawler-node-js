f89342d18504a443cbcdee29d7078f1fe8247dfb
= <devwellington@gmail.com>
2019-12-04 07:10:32 -0300

    Refactoring organization structure

--- app.js
+++ app.js
@@ -2,111 +2,58 @@ require('dotenv').config()
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-// axios.get(process.env.SEMINOVOS_VEICULOS)
-//     .then((response) => {
-//         const html = response.data
-//         const $ = cheerio.load(html)
-//         var devToList = []
-//         $('.list-of-cards .item').map(function(i,element) {
-
-//             let url = $(element).find('.card-content a').attr('href')
-//             url = url.substring(0,url.indexOf('?'))
-
-//             const id = url.split('-').reverse()[0]
-//             const figure = ($(element).find('figure a img').attr('src') ? 
-//                 $(element).find('figure a img').attr('src') : 
-//                 $(element).find('figure a img').attr('data-src'))
-
-//             devToList[i] = {
-//                 'id': id,
-//                 'url': 'https://seminovos.com.br' + url + '/',
-//                 'figure': figure,
-//                 'title': $(element).find('.card-content a .card-title').text().trim(),
-//                 'price': $(element).find('.card-content a .card-price').text().trim(),
-//                 'version': $(element).find('.card-content .card-info .card-subtitle span').text().trim(),
-//                 'vendorType': $(element).find('.card-content .card-info .card-owner-label').text().trim(),
-//                 'year': $(element).find('.card-content .card-info .card-features .list-features li[title="Ano de fabricação"]').text().trim(),
-//                 'km': $(element).find('.card-content .card-info .card-features .list-features li[title="Kilometragem atual"]').text().trim(),
-//                 'shift': $(element).find('.card-content .card-info .card-features .list-features li[title="Tipo de câmbio"]').text().trim(),
-//                 'list': $(element).find('.card-content .card-info .card-features .list-inline li').map((i,e) => 
-//                         $(e).text().trim().replace(',','')
-//                 ),
-                
-//             };
-
-//             // console.log(devToList)
-//         })
-//         console.log(devToList)
-//     },
-//     (err) => console.log(err)
-//     )
-
-
-// const html = '<div><h3 class="title">Teste rfasdfasjdfajs</h3></div>'
-// const $ = cheerio.load(html)
-
-// console.log($.html(),$('.title').text())
-
-
-// axios.get(process.env.OLX_VEICULOS)
-//     .then((response) => {
-//         const html = response.data
-//         const $ = cheerio.load(html)
-//         var devToList = []
-//         $('.section_OLXad-list .item').map(function(i,element) {
-
-//             devToList[i] = {
-//                 'title': $(element).find('.OLXad-list-link').find('.OLXad-list-title').text().trim(),
-//                 'price': $(element).find('.OLXad-list-link').find('.OLXad-list-price').text().trim(),
-//                 'image': $(element).find('.OLXad-list-link').find('.OLXad-list-image-box').find('img.image').attr('src'),
-//                 // 'url': $(element).find('.story-link').attr('href')
-//             };
-
-//             // console.log(devToList)
-//         })
-//         console.log(devToList)
-//     },
-//     (err) => console.log(err)
-//     )
-
 function getHTML() {
-    const html = axios.get(process.env.SEMINOVOS_VEICULOS).then((response) => response.data)
-    return html
+    try {
+        const html = axios.get(process.env.SEMINOVOS_VEICULOS)
+            .then((response) => response.data)
+            .catch((error) => console.error(error))
+    
+        return Promise.resolve(html)        
+    } catch (error) {
+        return Promise.reject(error)
+    }
+}
+
+function convertHTML(html) {
+    
+    const $ = cheerio.load(html)
+    const result = $('.list-of-cards .item').map(function (i, element) {
+        let url = $(element).find('.card-content a').attr('href')
+        url = url.substring(0, url.indexOf('?'))
+
+        const id = url.split('-').reverse()[0]
+        const figure = ($(element).find('figure a img').attr('src') ?
+            $(element).find('figure a img').attr('src') :
+            $(element).find('figure a img').attr('data-src'))
+
+        return [{
+            'id': parseInt(id),
+            'url': 'https://seminovos.com.br' + url + '/',
+            'figure': figure,
+            'title': $(element).find('.card-content a .card-title').text().trim(),
+            'price': parseFloat($(element).find('.card-content a .card-price').text().trim().replace('R$ ','')),
+            'version': $(element).find('.card-content .card-info .card-subtitle span').text().trim(),
+            'vendorType': $(element).find('.card-content .card-info .card-owner-label').text().trim(),
+            'year': $(element).find('.card-content .card-info .card-features .list-features li[title="Ano de fabricação"]').text().trim(),
+            'km': parseFloat($(element).find('.card-content .card-info .card-features .list-features li[title="Kilometragem atual"]').text().trim().replace(' km','')),
+            'shift': $(element).find('.card-content .card-info .card-features .list-features li[title="Tipo de câmbio"]').text().trim(),
+            'list': $(element).find('.card-content .card-info .card-features .list-inline li').map((i, e) =>
+                $(e).text().trim().replace(',', '')
+            ).get(),
+        }]
+
+    }).get()
+
+    return Promise.resolve(result)
 }
 
 async function main() {
 
     try {
         const html = await getHTML()
-        const $ = cheerio.load(html)
-        const result = $('.list-of-cards .item').map(function (i, element) {
-            let url = $(element).find('.card-content a').attr('href')
-            url = url.substring(0,url.indexOf('?'))
-
-            const id = url.split('-').reverse()[0]
-            const figure = ($(element).find('figure a img').attr('src') ? 
-                $(element).find('figure a img').attr('src') : 
-                $(element).find('figure a img').attr('data-src'))
-
-            return [{
-                'id': id,
-                'url': 'https://seminovos.com.br' + url + '/',
-                'figure': figure,
-                'title': $(element).find('.card-content a .card-title').text().trim(),
-                'price': $(element).find('.card-content a .card-price').text().trim(),
-                'version': $(element).find('.card-content .card-info .card-subtitle span').text().trim(),
-                'vendorType': $(element).find('.card-content .card-info .card-owner-label').text().trim(),
-                'year': $(element).find('.card-content .card-info .card-features .list-features li[title="Ano de fabricação"]').text().trim(),
-                'km': $(element).find('.card-content .card-info .card-features .list-features li[title="Kilometragem atual"]').text().trim(),
-                'shift': $(element).find('.card-content .card-info .card-features .list-features li[title="Tipo de câmbio"]').text().trim(),
-                'list': $(element).find('.card-content .card-info .card-features .list-inline li').map((i,e) => 
-                        $(e).text().trim().replace(',','')
-                ),
-            }]
-
-        })
-        console.log(result)
+        const result = await convertHTML(html)
         
+        console.log(result)        
     } catch (error) {
         console.error('Error: ', error)
     }
